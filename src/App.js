45fc3a41c2e8a3c017daa5a6f82835c697344f75
Lukas Londono <152759905+lukaslondono77@users.noreply.github.com>
2025-07-11 13:57:53 -0500

    Remove Document Library section from website

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import UseCaseGallery from './components/UseCaseGallery';
 import AIBIMAction from './components/AIBIMAction';
 import TechnologyStack from './components/TechnologyStack';
 import BenefitsSection from './components/BenefitsSection';
-import DocumentLibrary from './components/DocumentLibrary';
 import InteractiveCharts from './components/InteractiveCharts';
 import RealWorldUseCases from './components/RealWorldUseCases';
 import ExpertProfile from './components/ExpertProfile';
@@ -22,7 +21,6 @@ function App() {
       <UseCaseGallery />
       <AIBIMAction />
       <TechnologyStack />
-      <DocumentLibrary />
       <RealWorldUseCases />
       <InteractiveCharts />
       <ExpertProfile />
